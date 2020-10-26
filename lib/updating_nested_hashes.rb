0f633494f99684d5024a934dd61ab9d3089972a1
Jaime David <jaimedavid01@yahoo.com>
2020-10-26 20:02:12 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -61,7 +61,8 @@ def add_characters
    #   - The second hash should have the :name key set to "Apothecary" 
    # Code your solution here:
 
-if epic_tragedy[:additional_characters][]
+if epic_tragedy[:additional_characters][:name]
+  
 
 
 
